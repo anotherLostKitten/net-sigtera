63ec4c1599c924f9d41887a9aab00c5cde840932
Matteo Nunez <matteo.nunez22@gmail.com>
2022-04-01 19:51:46 -0400

    Add compression method in mode 2, tweak denoise values

--- a2/fft.py
+++ a2/fft.py
@@ -1,3 +1,10 @@
+"""
+ECSE 316 Assignment 2 Fast Fourier Transform and Applications
+Group 46
+Theodore Peters (260919785)
+Matteo Nunez (260785867)
+"""
+
 import cv2
 import cmath
 from math import pi, log, ceil
@@ -122,7 +129,7 @@ def denoise(img, amt):
     dc = round(c/2*amt)
     dr = round(r/2*amt)
     num_zeros = 2*dc*r + 2*dr*c - 4*dr*dc
-    print(f"Using {img.size-num_zeros} non-zeroes; {(img.size-num_zeros)*100/img.size}% of original fourier coefficients")
+    print(f"Using {img.size-num_zeros} non-zeroes; {round((img.size-num_zeros)*100/img.size, 3)}% of original fourier coefficients")
     img[c//2-dc:c//2+dc,:] = np.zeros((dc*2, r), dtype = 'complex_')
     img[:,r//2-dr:r//2+dr] = np.zeros((c, dr*2), dtype = 'complex_')
     return img
@@ -147,28 +154,51 @@ if __name__ == "__main__":
     
     if mode == 1:
         fig, (plt_orig, plt_dft, plt_np_fft) = plt.subplots(1, 3)
-        fig.suptitle("Log scaled |DFT|")
+        fig.suptitle("Mode 1: Log scaled |DFT|")
+        plt_orig.set_title("Original")
+        plt_dft.set_title("FFT Transform")
+        plt_np_fft.set_title("Built-in np.fft.fft2")
         img_dft = ctdft(img_scaled)
         img_np_fft = np.fft.fft2(img_scaled)
-        plt_orig.imshow(img_scaled, cmap="gray") # this is using the power 2 scaled image to display because that is what is being used to fetch the DFT values
-        show_normalized(plt_dft, img_dft) # again, don't scale image back to original ratio, show most accurate DFT display
-        show_normalized(plt_np_fft, img_np_fft)
+        plt_orig.imshow(img, cmap="gray") # this is using the power 2 scaled image to display because that is what is being used to fetch the DFT values
+        show_normalized(plt_dft, img_dft, orig_size) # again, don't scale image back to original ratio, show most accurate DFT display
+        show_normalized(plt_np_fft, img_np_fft, orig_size)
 
     elif mode == 2:
-        denoise_amount = 0.8 # amount to denoise image by (see denoise function)
+        denoise_amount = 0.86 # amount to denoise image by (see denoise function)
+        compress_amount = 0.992 # amount to compress image by
         
-        fig, (plt_orig, plt_denoise) = plt.subplots(1, 2)
-        fig.suptitle("Denoising")
-        img_dft = denoise(ctdft(img_scaled), denoise_amount)
+        fig, (plt_orig, plt_denoise, plt_compress) = plt.subplots(1, 3)
+        fig.suptitle("Mode 2: Denoising")
+        plt_orig.set_title("Original")
+        plt_denoise.set_title(f"Denoised ({denoise_amount*100}% cut off)")
+        plt_compress.set_title(f"Denoised ({compress_amount*100}% compressed)")
+    
+        # Original
         plt_orig.imshow(img, cmap="gray")
+
+        # Cutoff method
+        img_dft = denoise(ctdft(img_scaled), denoise_amount)
         img_denoise = cv2.resize(abs(ictdft(img_dft)), orig_size) # resize back image to preserve ratio with original
         plt_denoise.imshow(img_denoise, cmap="gray")
+
+        # Compression method
+        img_dft = ctdft(img_scaled)
+        threshs = abs(img_dft).flatten()
+        threshs.sort() # sort the values by magnitude to allow retrieving the nth percentile threshhold
+        ti = round(compress_amount*threshs.size) # the threshhold index comes from the compression amount as a fraction of all the possible sorted values; any higher values will be kept
+        thresh = threshs[ti]
+        print(f"Magnitude threshhold with {threshs.size - ti} non-zeroes; {round(100*(threshs.size - ti)/threshs.size, 3)}% of fourier coefficients remain")
+        f = np.vectorize(lambda c: c if abs(c)>=thresh else 0) # zero values who's magnitude is below threshhold
+        img_dft = f(img_dft)
+        img_comp = cv2.resize(abs(ictdft(img_dft)), orig_size) # resize back image to preserve ratio with original
+        plt_compress.imshow(img_comp, cmap="gray")
 
     elif mode == 3:
         compress_amounts = ((0, 0.5, 0.75), (0.9, 0.99, 0.999)) # compression ratios for each image, in reading order. note actual compression amounts may vary somewhat with inexact numbers of pixels to represent
         
         fig, plts = plt.subplots(2, 3)
-        fig.suptitle("Compression")
+        fig.suptitle("Mode 3: Compression")
         img_dft = ctdft(img_scaled)
         threshs = abs(img_dft).flatten()
         threshs.sort() # sort the values by magnitude to allow retrieving the nth percentile threshhold
@@ -177,14 +207,16 @@ if __name__ == "__main__":
                 if i==0 and j==0: # the first compression amount will always be ignored, just print original image
                     print(f"Original image: {threshs.size} non-zeroes")
                     plts[i][j].imshow(img, cmap="gray")
+                    plts[i][j].set_title("Original")
                     continue
                 ti = round(compress_amounts[i][j]*threshs.size) # the threshhold index comes from the compression amount as a fraction of all the possible sorted values; any higher values will be kept
                 thresh = threshs[ti]
-                print(f"Magnitude threshhold with {threshs.size - ti} non-zeroes; {100*(threshs.size - ti)/threshs.size}% of fourier coefficients remain")
+                print(f"Magnitude threshhold with {threshs.size - ti} non-zeroes; {round(100*(threshs.size - ti)/threshs.size, 3)}% of fourier coefficients remain")
                 f = np.vectorize(lambda c: c if abs(c)>=thresh else 0) # zero values who's magnitude is below threshhold
                 img_dft = f(img_dft)
                 img_comp = cv2.resize(abs(ictdft(img_dft)), orig_size) # resize back image to preserve ratio with original
                 plts[i][j].imshow(img_comp, cmap="gray")
+                plts[i][j].set_title(f"{compress_amounts[i][j]*100}% Compression")
         
     elif mode == 4:
         rng = np.random.default_rng()
@@ -196,9 +228,9 @@ if __name__ == "__main__":
         for s in sizes:
             dft_runs = []
             ctdft_runs = []
-            print(f"Matrix size {s}x{s}") # progress output so program doesn't appear to hang
+            print(f"Matrix Size {s}x{s}") # progress output so program doesn't appear to hang
             for t in range(0,10):
-                print(f"\tRun {t+1}") # progress output so program doesn't appear to hang
+                # print(f"\tRun {t+1}") # progress output so program doesn't appear to hang
                 a = rng.random((s, s))
                 start = timeit.default_timer()
                 dft(a)
@@ -213,12 +245,14 @@ if __name__ == "__main__":
             dft_dev.append(2*np.std(dft_runs))
             ctdft_avg.append(sum(ctdft_runs)/10)
             ctdft_dev.append(2*np.std(ctdft_runs))
-            print(f"DFT Average: {dft_avg[-1]} \tDFT Stand. dev.: {dft_dev[-1]}")
-            print(f"CTDFT Average: {ctdft_avg[-1]} \tCTDFT Stand. dev.: {ctdft_dev[-1]}\r")
+            print(f"DFT Average: {round(dft_avg[-1], 6)} \t\tDFT Stand. dev.: {round(dft_dev[-1], 6)}")
+            print(f"CTDFT Average: {round(ctdft_avg[-1], 6)} \tCTDFT Stand. dev.: {round(ctdft_dev[-1], 6)}")
+            print("")
         plt.errorbar(sizes, dft_avg, dft_dev, ecolor='k', label="Naive DFT")
         plt.errorbar(sizes, ctdft_avg, ctdft_dev, ecolor='k', label="Cooley Tukey DFT")
         plt.legend()
-        plt.xlabel("matrix dimension (NxN)")
-        plt.ylabel("time (s)")
+        plt.xlabel("Problem Size (matrix dimension) (NxN)")
+        plt.ylabel("Average Runtime (s)")
+        plt.suptitle("Mode 4: Runtime over Problem Size for Naive DFT & Cooley Tukey DFT algorithms")
     
     plt.show()
